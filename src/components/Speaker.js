caef2adb34bed475405df90e650116a50329f9ed
ddord <ikihiji11@gmail.com>
2021-06-28 23:21:41 +0900

    11

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -33,11 +33,23 @@ function Session({title, room}){
       <div className="speaker-info">
         <div className="d-flex justify-content-between mb-3">
           <h3 className="text-truncate w-200">
-            {first} {last} {first}
+            {first} {last}
           </h3>
         </div>
         <div>
-          <p>{bio} {company} {twitterHandle} {favorite}</p>
+          <p className="card-description">
+            {bio}
+          </p>
+          <div className="social d-flex flex-row mt-4">
+            <div className="company">
+              <h5>Company</h5>
+              <h6>{company}</h6>
+            </div>
+            <div className="twitter">
+              <h5>Twitter</h5>
+              <h6>{twitterHandle}</h6>
+            </div>
+          </div>
         </div>
       </div> 
     );
