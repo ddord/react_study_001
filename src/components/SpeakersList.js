3ad9824152dbd3d9ad4d904532c84a72b08a7096
ddord <ikihiji11@gmail.com>
2021-08-15 22:45:57 +0900

    11

--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -22,7 +22,6 @@ function SpeakersList({ showSessions }) {
     }
 
     //if (isLoading === true) return <div>Loading...</div>
-
     return (
         <div className="continer speakers-list">
             <ReactPlaceholder
